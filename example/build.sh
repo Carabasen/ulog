dbfe970d18c1a2afa7528fbb4704135f95ebbac8
Pavel Alekseev <carabasen@gmail.com>
2020-07-13 23:17:34 +0300

    upd: add appveyor ci script

--- example/build.sh
+++ example/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-g++ -std=c++17 -Werror -Wpedantic -Wall -Wextra -O3 -pthread -o ulog_example ../ulog.cpp ulog_example.cpp
+g++ -std=c++17 -lstdc++fs -Werror -Wpedantic -Wall -Wextra -O3 -pthread -o ulog_example ../ulog.cpp ulog_example.cpp
 
 #./ulog_example
 
